400d41689e83df000bc9d43564c76ed344613532
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2023-09-17 18:41:26 -0700

    add tf-idf matrix

--- search_engine.py
+++ search_engine.py
@@ -10,6 +10,7 @@
 
 #importing some Python libraries
 import csv
+import math
 
 documents = []
 labels = []
@@ -22,7 +23,6 @@ with open('collection.csv', 'r') as csvfile:
             documents.append (row[0])
             labels.append(row[1])
             
-print(documents)
 
 
 #Conduct stopword removal.
@@ -39,7 +39,7 @@ for i in range(len(documents)):
         word_list.remove(word)
   documents[i] = " ".join(word_list)
   
-print(documents)
+
         
 
 
@@ -59,17 +59,72 @@ for i in range(len(documents)):
       word_list[w] = steeming[word_low]
   documents[i] = " ".join(word_list)
   
-print(documents)
+print("Documents",documents)
 
   
 
 #Identify the index terms.
 #--> add your Python code here
 terms = []
+for i in range(len(documents)):
+  word_list = documents[i].split()
+  for w in range(len(word_list)):
+    world_low = word_list[w].lower()
+    if world_low not in terms:
+      terms.append(world_low)
 
 #Build the if-idf term weights matrix.
 #--> add your Python code here
 docMatrix = []
+
+word_count = {}
+for i in range(len(documents)):
+  word_list = documents[i].split()
+  for w in range(len(word_list)):
+    world_low = word_list[w]
+    if world_low in word_count:
+      word_count[world_low] += 1
+    else:
+      word_count[world_low] = 1
+
+doc_count = {}
+for i in range(len(documents)):
+  word_list = documents[i].split()
+  for key in word_count:
+    if key in word_list and key in doc_count:
+      doc_count[key] += 1
+    elif key in word_list and key not in doc_count:
+      doc_count[key] = 1
+
+def calcTF(doc_index, word):
+  temp = documents[doc_index].split()
+  doc_len = len(temp)
+  freq = temp.count(word)
+  return freq/doc_len
+
+def calcIDF(word):
+  return math.log10(len(documents)/doc_count[word])
+
+for i in range(len(documents)):
+  new_row = [0 for i in range(len(terms))]
+  docMatrix.append(new_row)
+  
+#print(docMatrix)
+
+
+for i in range(len(documents)):
+  word = terms[i]
+  col_idf = calcIDF(word)
+  #print(col_idf)
+  for j in range(len(terms)):
+    row_tf = calcTF(j,word)
+    #print(row_tf)
+    docMatrix[j][i] = round(col_idf * row_tf,4)
+    
+print(docMatrix)
+  
+  
+
 
 #Calculate the document scores (ranking) using document weigths (tf-idf) calculated before and query weights (binary - have or not the term).
 #--> add your Python code here
